d5f17aecbf37854675a0ad6abbc3ee0981022cfc
Jakub Kozler <kozlerjakub@gmail.com>
2023-11-11 17:53:39 +0100

    added Toggle Component

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import TestModal from "./components/Modal/TestModal";
 
 export default function App() {
-    return <h1>Skisafe</h1>;
+    return (
+        <>
+            <h1>Skisafe</h1>
+
+            <TestModal />
+        </>
+    );
 }
