ca8ccb436c0fcaa341f9f4c84be92bc3cecb982e
Jakub Kozler <kozlerjakub@gmail.com>
2023-11-12 14:14:14 +0100

    added map to homepage

--- resources/js/components/homepage/homepage.jsx
+++ resources/js/components/homepage/homepage.jsx
@@ -1,28 +1,26 @@
-import './homepage.scss'
-import Footer from './footer/Footer'
-import MainContent from './mainContext/MainContext'
-import Navigation from './navigation/Navigation'
-import AboutUs from './aboutUs/AboutUs'
-import ContactUs from './contactUs/ContactUs'
-import { Route, Routes } from 'react-router-dom'
+import "./homepage.scss";
+import Footer from "./footer/Footer";
+import MainContent from "./mainContent/MainContent";
+import Navigation from "./navigation/Navigation";
+import AboutUs from "./aboutUs/AboutUs";
+import ContactUs from "./contactUs/ContactUs";
+import { Route, Routes } from "react-router-dom";
+import Map from "../../Map/Map";
 
-export default function Homepage(){
+export default function Homepage() {
+    return (
+        <>
+            <Navigation />
 
-    return(
-        
-            <>
-                <Navigation />
-                
-                    <Routes >
-                        
-                        <Route path="/" element={ <MainContent />} ></Route>
-                        <Route path="/about-us" element={ <AboutUs /> } ></Route>
-                        <Route path="/contact-us" element={ <ContactUs />} ></Route>
-    
-                    </Routes>
-                
-                <Footer />
-            </>
-    
-    )
-} 
+            <Routes>
+                <Route path="/" element={<MainContent />}></Route>
+                <Route path="/about-us" element={<AboutUs />}></Route>
+                <Route path="/contact-us" element={<ContactUs />}></Route>
+            </Routes>
+
+            <Map />
+
+            <Footer />
+        </>
+    );
+}
