8057a32ba66d094edc1e2094e88f25ca38decd54
Damaris Jean <Damarisjjm@icloud.com>
2023-11-14 15:54:16 +0100

    damaris redirecting to homepage

--- resources/js/components/homepage/login/Login.jsx
+++ resources/js/components/homepage/login/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import UserContext from "../../../context/UserContext";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function Login(props) {
     const [values, setValues] = useState({
@@ -12,7 +13,7 @@ export default function Login(props) {
 
     const { user, setUser } = useContext(UserContext);
 
-    const [userName, setUserName] = useState("");
+    const navigate = useNavigate();
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -22,22 +23,18 @@ export default function Login(props) {
             console.log(response);
 
             setUser(null);
-
-            if (response.status == 200) {
+            if (response.status === 200) {
                 const paragraph = document.querySelector(".success-login");
                 paragraph.style.display = "block";
+                navigate("/");
             }
         } catch (error) {
-            switch (error.response.status) {
-                case 422:
-                    console.log(
-                        "VALIDATION FAILED:",
-                        error.response.data.errors
-                    );
-                    break;
-                case 500:
-                    console.log("UNKNOWN ERROR", error.response.data);
-                    break;
+            if (error.response && error.response.status === 422) {
+                console.log("VALIDATION FAILED:", error.response.data.errors);
+                setErrors(error.response.data.errors);
+            } else {
+                console.log("ERROR", error);
+                setErrors({ general: ["An unexpected error occurred."] });
             }
         }
     };
@@ -96,7 +93,7 @@ export default function Login(props) {
             <button>Login</button>
             <p className="success-login">
                 {user
-                    ? `Hello ${user?.name}, you have successfully logged in.`
+                    ? `Hello ${user.name}, you have successfully logged in.`
                     : ""}
             </p>
         </form>
--- resources/js/components/homepage/register/Register.jsx
+++ resources/js/components/homepage/register/Register.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import UserContext from "../../../context/UserContext";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function Register(props) {
     const [values, setValues] = useState({
@@ -15,6 +16,8 @@ export default function Register(props) {
 
     const { setUser } = useContext(UserContext);
 
+    const navigate = useNavigate();
+
     const handleSubmit = async (event) => {
         event.preventDefault();
 
@@ -23,22 +26,23 @@ export default function Register(props) {
 
         try {
             const response = await axios.post("/register", values);
-            
-            setUser(null);
+            navigate("/");
+
+            setUser(response.data.user);
         } catch (error) {
-            switch (error.response.status) {
-                case 422:
-                    console.log(
-                        "VALIDATION FAILED:",
-                        error.response.data.errors
-                    );
-                    break;
-                case 500:
-                    console.log("UNKNOWN ERROR", error.response.data);
-                    break;
+            if (error.response && error.response.status === 422) {
+                console.log(
+                    "VALIDATION FAILED:",
+                    error.response.data.errors,
+                    setErrors(error.response.data.errors)
+                );
+            } else {
+                console.log("ERROR", error);
+                setErrors({ general: ["An unexpected error occurred."] });
             }
         }
     };
+
     const handleChange = (event) => {
         setValues((previous_values) => {
             return {
