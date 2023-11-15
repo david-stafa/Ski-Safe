4c42f7765bf298b2eaa3b9e544ee890571ed5133
Joe <joetkane@gmail.com>
2023-11-15 15:26:00 +0000

    working pop ups for each pin

--- resources/js/Map/popUp/showPopUp.js
+++ resources/js/Map/popUp/showPopUp.js
@@ -1,7 +1,10 @@
 import mapboxgl from "mapbox-gl";
+import { getPins } from "../Pins/getPins";
 import { pinOnMap } from "../Pins/addPinOnMap";
 
-export const popUp = (map) => {
+export const ShowPopUp = async (map) => {
+    // const pinData = await getPins();
+
     const myPopUp = new mapboxgl.Popup({
         closeButton: false,
         closeOnClick: true,
@@ -11,23 +14,18 @@ export const popUp = (map) => {
         // when we click points, create a pop up at the coordinates and use the description included in the geoJson.
         const coordinates = e.features[0].geometry.coordinates.slice();
         const title = e.features[0].properties.title;
-        const description = e.features[0].properties.description;
+        const slug = e.features[0].properties.slug;
         const severity = e.features[0].properties.severity;
-        const image = e.features[0].properties.image;
 
         myPopUp
             .setLngLat(coordinates)
             .setHTML(
                 `<h3 style="color: #333; font-size: 16px;">${title}</h3>
-                    <p style="color: #666; font-size: 14px;">${description}</p>
+                    <p style="color: #666; font-size: 14px;">${slug}</p>
                     <p style="color: #f00; font-size: 14px;">
                         Severity: ${severity}
                     </p>
-                    <img
-                        src="${image}"
-                        alt="${title}"
-                        style="max-width: 100%; height: auto; margin-top: 10px;"
-                    /> `
+                    <button>Details</button`
             )
             .addTo(map);
     });
@@ -40,6 +38,5 @@ export const popUp = (map) => {
     // remove mouse style as user leaves
     map.on("mouseleave", "points", () => {
         map.getCanvas().style.cursor = "";
-
     });
 };
