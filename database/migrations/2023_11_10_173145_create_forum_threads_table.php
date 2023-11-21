e644817bc2664d36868803b76f9937f5295b95a9
David Štafa <david.stafa@gmail.com>
2023-11-21 15:54:33 +0100

    forum in progress + weather style change

--- database/migrations/2023_11_10_173145_create_forum_threads_table.php
+++ database/migrations/2023_11_10_173145_create_forum_threads_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('forum_threads', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->string('content');
             $table->unsignedBigInteger('user_id'); 
             $table->timestamps(); 
         });
